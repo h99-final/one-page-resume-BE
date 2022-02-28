02f9d1baa4be748d3a785a81e09b10d6cf624210
고영빈 <45589210+skekq123@users.noreply.github.com>
2022-02-28 23:39:03 +0900

    intial commit

---
+++ src/main/java/com/f5/onepageresumebe/OnePageResumeBeApplication.java
@@ -0,0 +1,13 @@
+package com.f5.onepageresumebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePageResumeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnePageResumeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f5/onepageresumebe/OnePageResumeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.onepageresumebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePageResumeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
