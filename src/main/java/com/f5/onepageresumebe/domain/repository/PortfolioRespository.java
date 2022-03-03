45a882d96c0fb653c1de1eca4779c649392d5d28
guswls159357 <guswls159357@naver.com>
2022-03-03 16:33:14 +0900

    필요 없는 파일 삭제

--- src/main/java/com/f5/onepageresumebe/domain/repository/PortfolioRespository.java
+++
@@ -1,7 +0,0 @@
-package com.f5.onepageresumebe.domain.repository;
-
-import com.f5.onepageresumebe.domain.entity.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRespository extends JpaRepository<Portfolio, Integer> {
-}
--- src/main/java/com/f5/onepageresumebe/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.f5.onepageresumebe.repository;
-
-import com.f5.onepageresumebe.domain.entity.PortfolioStack;
-import com.f5.onepageresumebe.domain.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
