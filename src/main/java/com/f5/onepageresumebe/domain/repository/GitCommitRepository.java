602cfc5b71ba98c1ac8f27f7deb1bbe3722f20ef
guswls159357 <guswls159357@naver.com>
2022-03-07 14:48:07 +0900

    쿼리 추가

--- src/main/java/com/f5/onepageresumebe/domain/repository/GitCommitRepository.java
+++ src/main/java/com/f5/onepageresumebe/domain/repository/GitCommitRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface GitCommitRepository extends JpaRepository<GitCommit, Integer> {
     GitCommit findBySha(String sha);
