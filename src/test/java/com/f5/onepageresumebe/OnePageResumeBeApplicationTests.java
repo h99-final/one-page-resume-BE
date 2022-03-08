b00725211855da3dffe611cdbf0fb0340190cb02
kjhbbjoker <whdgns4288@naver.com>
2022-03-08 16:02:14 +0900

    무중단 서버 테스트중

--- src/test/java/com/f5/onepageresumebe/OnePageResumeBeApplicationTests.java
+++ src/test/java/com/f5/onepageresumebe/OnePageResumeBeApplicationTests.java
@@ -3,11 +3,11 @@ package com.f5.onepageresumebe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OnePageResumeBeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    //@Test
+  //  void contextLoads() {
+  //  }
 
 }
