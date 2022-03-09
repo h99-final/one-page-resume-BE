ecacd0531905dc26145051821eb0077c5df55fc8
kjhbbjoker <whdgns4288@naver.com>
2022-03-09 16:28:08 +0900

    테스트중입니다!!@!

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+# switch.sh
+# nginx 연결 설정 스위치
+
+# profile import
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
+  # 유휴 포트 저장
     IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
-    # 아래 줄은 보면 echo를 통해서 나온 결과를 | 파이프라인을 통해서 service-url.inc에 덮어쓸 수 있습니다.
-    echo "set \$service_url http://13.124.241.74:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    # nginx와 연결한 주소 생성
+    # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
     echo "> 엔진엑스 Reload"
-    sudo service nginx reload    # Nginx reload를 합니다.
+    # nignx reload. restart와는 다르게 설정 값만 불러옴 (restart는 전체를 다시 재시작하기때문에 상대적으로 느림)
+    sudo service nginx reload
 }
