066fef6dbfc156cd986fc0c8a6c8f0d66844c32b
kjhbbjoker <whdgns4288@naver.com>
2022-03-10 01:51:00 +0900

    제발요gk

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-echo "> 현재 구동중인  Port 확인"
-CURRENT_PROFILE=$(curl -s http://localhost/profile)
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh # 자바 import와 동일
 
-if [ $CURRENT_PROFILE == dev ]
-then
-  IDLE_PORT=8082
-elif [ $CURRENT_PROFILE == dev2 ]
-then
-  IDLE_PORT=8081
-else
-  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
-  echo "> 8081을 할당합니다."
-  IDLE_PORT=8081
-fi
+function switch_proxy() {
+  IDLE_PORT=$(find_idle_port)
 
-echo "> 전환할 Port: $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "> 전환할 Port: $IDLE_PORT"
+  echo "> Port 전환"
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc #tee: 파일에도 쓰고 표준출력도 한다.
 
-PROXY_PORT=$(curl -s http://localhost/profile)
-echo "> Nginx Current Proxy Port: $PROXY_PORT"
-
-echo "> Nginx Reload"
-sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
+  echo "> 엔진엑스 Reload"
+  sudo service nginx reload # restart는 끊김이 있지만 reload는 끊김이 없다.
+}
