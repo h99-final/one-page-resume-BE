d86aafd9fe851decc4bcf6dbfec4e15804932aad
kjhbbjoker <whdgns4288@naver.com>
2022-03-10 11:24:00 +0900

    제발요gk

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
+# start.sh
+# 서버 구동을 위한 스크립트
 
+# 절대경로를 이용하여 profile.sh 경로 찾은 후 import
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh # 자바 import와 동일
+source ${ABSDIR}/profile.sh
 
+
+# 현재 프로젝트 경로 지정
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=blogProject
+IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
 
-echo "> 빌드 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
+echo "> JASYPT_ENCRYPTOR_PASSWORD: ${JASYPT_ENCRYPTOR_PASSWORD}"
+echo "> IDLE_PORT : $IDLE_PORT"
+echo "> IDLE_PROFILE : $IDLE_PROFILE"
 
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+# 새로운 jar file 덮어쓰기
 cp $REPOSITORY/zip/*.jar $REPOSITORY
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -S $REPOSITORY/*.jar | head -n 1)
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-IDLE_PROFILE=$(find_idle_profile)
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
+cd $REPOSITORY
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
