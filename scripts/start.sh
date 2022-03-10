cd0992e45035661ea0100661b626e6e1e60a65b8
kjhbbjoker <whdgns4288@naver.com>
2022-03-10 19:19:28 +0900

    제발...

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,38 +4,31 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ubuntu/app/step
-IMAGEPATH=/home/ubuntu/app
-
-echo "> 이미지 저장 폴더 만들기"
-sudo mkdir ~/app/imageCollection
+REPOSITORY=/home/ubuntu/app/step3
+PROJECT_NAME=FoodMap
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/skiFriend-0.0.1-SNAPSHOT.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-sudo cp $REPOSITORY/zip/skiFriend-0.0.1-SNAPSHOT.jar $REPOSITORY/
-
-sudo rm -rf $REPOSITORY/nohup.out
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/skiFriend-0.0.1-SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    -Duser.dir=$IMAGEPATH \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 
