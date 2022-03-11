08b2c0f34ad9519d30b398969b5d9ee1d892b2b6
kjhbbjoker <whdgns4288@naver.com>
2022-03-11 17:13:24 +0900

    stat.sh 변경

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-# switch.sh
-# nginx 연결 설정 스위치
-
-# profile import
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
-  # 유휴 포트 저장
     IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
-    # nginx와 연결한 주소 생성
-    # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
+    # 포트 전환하면서, 바꾼거말고 다른거 kill
+    if [ ${IDLE_PORT} == 8081 ]
+    then
+      KILL_PORT=8082
+      IDLE_PID=$(lsof -ti tcp:${KILL_PORT})
+      echo "> ${KILL_PORT} 포트를 종료합니다."
+      kill -15 ${IDLE_PID}
+    else
+      KILL_PORT=8081
+      IDLE_PID=$(lsof -ti tcp:${KILL_PORT})
+      echo "> ${KILL_PORT} 포트를 종료합니다."
+      kill -15 ${IDLE_PID}
+    fi
+
     echo "> 엔진엑스 Reload"
-    # nignx reload. restart와는 다르게 설정 값만 불러옴 (restart는 전체를 다시 재시작하기때문에 상대적으로 느림)
     sudo service nginx reload
 }
