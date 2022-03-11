5d0234e31c510f86dff8dbc313c3797ee9dc0e46
kjhbbjoker <whdgns4288@naver.com>
2022-03-11 11:50:34 +0900

    되라됟라

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,29 +5,33 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ubuntu/app/step3
+IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
 IMAGEPATH=/home/ubuntu/app
+
+
+echo "> IDLE_PORT : $IDLE_PORT"
+echo "> IDLE_PROFILE : $IDLE_PROFILE"
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/.jar | tail -n 1)
+JAR_NAME=$(ls -S $REPOSITORY/*.jar | head -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-#
-IDLE_PROFILE=$(find_idle_profile)
-##
+
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-cd$REPOSITORY
+cd $REPOSITORY
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
