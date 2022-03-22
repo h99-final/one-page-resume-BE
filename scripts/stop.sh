66386721fd13d5939be24d709bba3be561ae0e42
kjhbbjoker <whdgns4288@naver.com>
2022-03-22 23:09:50 +0900

    복호화 테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -9,7 +9,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $IDLE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PROFILE})
-
+#
 if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
