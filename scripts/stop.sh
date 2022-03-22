a9689c6e3a07fbc0956df0b2f35ddc25977f4950
kjhbbjoker <whdgns4288@naver.com>
2022-03-22 23:05:49 +0900

    복호화 테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,8 +7,8 @@ source ${ABSDIR}/profile.sh
 IDLE_PORT=$(find_idle_port)
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+echo "> $IDLE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PROFILE})
 
 if [ -z ${IDLE_PID} ]
 then
