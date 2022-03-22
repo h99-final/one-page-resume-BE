16819d6b927e39bcac2074667c45ab5752d7a17c
kjhbbjoker <whdgns4288@naver.com>
2022-03-22 18:52:41 +0900

    git hub workflows 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,5 +33,6 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 cd /home/ubuntu/app/step3
 
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE password=$JASYPT_ENCRYPTOR_PASSWORD one-page-resume-BE-0.0.1-SNAPSHOT.jar  > nohup.out 2>&1 &
+java -jar -Dspring.profiles.active=$IDLE_PROFILE one-page-resume-BE-0.0.1-SNAPSHOT.jar
+# > nohup.out 2>&1 &
 #nohup java -jar /home/ubuntu/app/step3/aws-exercise-0.0.1-SNAPSHOT.jar /dev/null 2> /dev/null < /dev/null
