0711d5ab1f4a4b50b8a64f8b835eec53d547de61
고영빈 <45589210+skekq123@users.noreply.github.com>
2022-03-25 01:13:47 +0900

    [FEAT] Thread 기능 추가
    깃허브 api를 이용해 데이터 업로드 시, 멀티쓰레드 방식으로 업로드 ThreadPool 5로 설정

---
+++ src/main/java/com/f5/onepageresumebe/config/ThreadConfig.java
@@ -0,0 +1,21 @@
+package com.f5.onepageresumebe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class ThreadConfig {
+    @Bean
+    @Primary
+    public TaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setThreadNamePrefix("default_task_executor_thread");
+        executor.initialize();
+        return executor;
+    }
+}
