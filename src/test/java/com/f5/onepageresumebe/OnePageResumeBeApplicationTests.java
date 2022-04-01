cbaef1898ff807da9f2a6524cb0ed6a6e4969283
kjhbbjoker <97422693+kjhbbjoker@users.noreply.github.com>
2022-04-01 23:09:03 +0900

    [FIX] 잘못된 패키지 구조 변경

--- src/test/java/com/f5/onepageresumebe/OnePageResumeBeApplicationTests.java
+++ src/test/java/com/f5/onepageresumebe/OnePageResumeBeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.f5.onepageresumebe;
+///package com.f5.onepageresumebe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class OnePageResumeBeApplicationTests {
+//class OnePageResumeBeApplicationTests {
 
    // @Test
-    void contextLoads() {
-    }
+  //  void contextLoads() {
+  //  }
 
-}
+//}
